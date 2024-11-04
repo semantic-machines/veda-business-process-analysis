5537539f942043b9afa5b042556200cf02da4c85
Roman Karpov <roman.karpov@gmail.com>
2024-11-04 18:16:39 +0300

    update client app

--- src/js/ProcessEdit.js
+++ src/js/ProcessEdit.js
@@ -23,25 +23,26 @@ export default class ProcessEdit extends Component(HTMLElement) {
   render() {
     return html`
       <div class="sheet">
-        <h3 class="mb-0" about="v-bpa:BusinessProcess" property="rdfs:label"></h3>
-        <div class="mb-3">
-          <label class="form-label" about="rdfs:label" property="rdfs:label"></label>
-          <input type="text" class="form-control" is="${InputText}" about="${this.model.id}" data-property="rdfs:label">
-        </div>
+        <h3 about="v-bpa:BusinessProcess" property="rdfs:label"></h3>
         <div class="row">
           <div class="col-12 col-md-9">
             <div class="mb-3">
-              <label class="form-label" about="v-bpa:processDescription" property="rdfs:label"></label>
+              <label class="form-label fw-bold" about="rdfs:label" property="rdfs:label"></label>
+              <input type="text" class="form-control" is="${InputText}" about="${this.model.id}" data-property="rdfs:label">
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label fw-bold" about="v-bpa:processDescription" property="rdfs:label"></label>
               <textarea class="form-control" is="${Textarea}" about="${this.model.id}" data-property="v-bpa:processDescription" rows="3"></textarea>
             </div>
 
             <div class="mb-3">
-              <label class="form-label" about="v-bpa:responsibleDepartment" property="rdfs:label"></label>
+              <label class="form-label fw-bold" about="v-bpa:responsibleDepartment" property="rdfs:label"></label>
               <input type="text" class="form-control" is="${InputText}" about="${this.model.id}" data-property="v-bpa:responsibleDepartment">
             </div>
 
             <div class="mb-3">
-              <label class="form-label" about="v-bpa:processFrequency" property="rdfs:label"></label>
+              <label class="form-label fw-bold" about="v-bpa:processFrequency" property="rdfs:label"></label>
               <div class="input-group">
                 <input type="number" is="${InputInteger}" class="form-control" about="${this.model.id}" data-property="v-bpa:processFrequency">
                 <span class="input-group-text" about="v-bpa:TimesPerYear" property="rdfs:label"></span>
@@ -49,7 +50,7 @@ export default class ProcessEdit extends Component(HTMLElement) {
             </div>
 
             <div class="mb-3">
-              <label class="form-label" about="v-bpa:laborCosts" property="rdfs:label"></label>
+              <label class="form-label fw-bold" about="v-bpa:laborCosts" property="rdfs:label"></label>
               <div class="input-group">
                 <input type="number" is="${InputDecimal}" class="form-control" about="${this.model.id}" data-property="v-bpa:laborCosts">
                 <span class="input-group-text" about="v-bpa:HoursPerYear" property="rdfs:label"></span>
@@ -58,7 +59,7 @@ export default class ProcessEdit extends Component(HTMLElement) {
           </div>
           <div class="col-12 col-md-3 border-start border-secondary-subtle">
             <div class="mb-3">
-              <label class="form-label" about="v-bpa:processParticipant" property="rdfs:label"></label>
+              <label class="form-label fw-bold" about="v-bpa:processParticipant" property="rdfs:label"></label>
               <textarea class="form-control" is="${Textarea}" about="${this.model.id}" data-property="v-bpa:processParticipant" rows="3"></textarea>
             </div>
           </div>
