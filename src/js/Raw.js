304df67ef66fdcf77e0b3b014c4c5382e5c53515
Roman Karpov <roman.karpov@gmail.com>
2024-11-16 12:21:21 +0300

    edit raw json

--- src/js/Raw.js
+++ src/js/Raw.js
@@ -4,6 +4,7 @@ export default class Raw extends Component(HTMLElement) {
   static tag = 'bpa-raw';
 
   format = localStorage.getItem('Raw_format') === 'ttl' ? 'ttl' : 'json';
+  editing = false;
 
   toggleFormat() {
     this.format = this.format === 'ttl' ? 'json' : 'ttl';
@@ -11,30 +12,109 @@ export default class Raw extends Component(HTMLElement) {
     this.update();
   }
 
+  async toggleEdit() {
+    this.editing = !this.editing;
+    await this.update();
+    const textarea = this.querySelector('textarea');
+    textarea.focus();
+  }
+
+  async saveChanges() {
+    try {
+      const textarea = this.querySelector('textarea');
+      const json = JSON.parse(textarea.value);
+      await this.model.apply(json);
+      await this.model.save();
+      this.editing = false;
+      this.update();
+    } catch (error) {
+      console.error('Ошибка при сохранении:', error);
+      alert('Ошибка при сохранении изменений. Проверьте формат JSON.');
+    }
+  }
+
   render() {
     return html`
-      <div class="nav nav-underline mb-2 ms-3" role="group">
-        <button type="button" 
-          class="nav-link ${this.format === 'ttl' ? 'active disabled' : 'text-secondary'}" 
-          @click="toggleFormat">
-          TTL
-        </button>
-        <button type="button"
-          class="nav-link ${this.format === 'json' ? 'active disabled' : 'text-secondary'}"
-          @click="toggleFormat">
-          JSON
-        </button>
-      </div>
-      <div class="sheet">
-        <pre class="m-0"><code></code></pre>
-      </div>
+      <style>
+        .fullscreen-editor {
+          position: fixed;
+          top: 0.25em;
+          left: 0.25em;
+          right: 0.25em;
+          bottom: 0.25em;
+          z-index: 1000;
+        }
+        .fullscreen-editor textarea {
+          font-size: 0.85em;
+          width: 100%;
+          height: calc(100%);
+          border: none;
+          padding: 1em 1em 2em 1em;
+        }
+        .bottom-controls {
+          position: fixed;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          z-index: 1001;
+          padding: 1em;
+          text-align: center;
+        }
+        .bottom-controls button {
+          margin: 0 0.5em;
+        }
+      </style>
+      ${this.editing ? html`
+        <div class="fullscreen-editor">
+          <textarea class="font-monospace form-control"></textarea>
+          <div class="bottom-controls">
+            <button type="button"
+              class="btn btn-success"
+              @click="saveChanges">
+              Сохранить
+            </button>
+            <button type="button"
+              class="btn btn-secondary"
+              @click="toggleEdit">
+              Отмена
+            </button>
+          </div>
+        </div>
+      ` : html`
+        <div class="mb-2 ms-3 d-flex justify-content-between">
+          <div class="nav nav-underline" role="group">
+            <button type="button" 
+              class="nav-link ${this.format === 'ttl' ? 'active disabled' : 'text-secondary'}" 
+              @click="toggleFormat">
+              TTL
+            </button>
+            <button type="button"
+              class="nav-link ${this.format === 'json' ? 'active disabled' : 'text-secondary'}"
+              @click="toggleFormat">
+              JSON
+            </button>
+          </div>
+          ${this.format === 'json' ? html`
+            <button type="button" class="btn text-dark" @click="toggleEdit">
+              <span about="v-bpa:Edit" property="rdfs:label"></span>
+            </button>
+          ` : ''}
+        </div>
+        <div class="sheet">
+          <pre class="m-0"><code></code></pre>
+        </div>
+      `}
     `;
   }
   
   post() {
-    this.querySelector('code').innerHTML = this.format === 'ttl' ? 
-      `${toTurtle(this.model)}` :
-      `${toJSON(this.model)}`;
+    if (this.editing) {
+      this.querySelector('textarea').value = toJSON(this.model);
+    } else {
+      this.querySelector('code').innerHTML = this.format === 'ttl' ? 
+        `${toTurtle(this.model)}` :
+        `${toJSON(this.model)}`;
+    }
   }
 }
 
